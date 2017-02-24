7788882029db6ded1bdc840827a29aa8fbc00944
Marek Onuszko <marek.onuszko@gmail.com>
2017-02-24 11:46:00 +0100

    frontend: paginate non-empty menucards, sorting options

--- karty/management/commands/create_sample_data.py
+++ karty/management/commands/create_sample_data.py
@@ -8,8 +8,9 @@ class Command(BaseCommand):
     help = "Populates the database with sample data to illustrate the app."
 
     def handle(self, *args, **options):
-        for dish_count in (10, 15, 21, 16, 9, 5, 44, 28, 13, 81, 33, 20, 19,
-                18, 12, 8, 14, 36, 11, 17, 14, 10, 10, 10, 12, 13, 15, 17, 18):
+        for dish_count in (10, 15, 21, 16, 0, 0, 9, 5, 44, 28, 13, 0, 81, 33,
+                20, 19, 18, 0, 12, 0, 8, 14, 36, 0, 11, 0, 0, 0, 17, 14, 10,
+                10, 10, 12, 13, 0, 15, 0, 17, 18):
             card = MenuCardFactory()
             DishFactory.create_batch(dish_count, menucard=card)
 
--- karty/views.py
+++ karty/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import MenuCard
 
 
 def menucard_list(request):
-    card_list = MenuCard.objects.all()
-    return render(request, 'karty/menucard_list.html', {'menucard_list': card_list})
+    sort_by = request.GET.get('sort_by')
+    sort_by = sort_by if sort_by in ('name', 'num_dishes') else 'pk'
+
+    card_list = MenuCard.objects.annotate(num_dishes=Count('dish'))
+    card_list = card_list.exclude(num_dishes=0).order_by(sort_by)
+
+    paginator = Paginator(card_list, 10)
+    page = request.GET.get('page')
+    try:
+        menucards = paginator.page(page)
+    except PageNotAnInteger:
+        menucards = paginator.page(1)
+    except EmptyPage:
+        menucards = paginator.page(paginator.num_pages)
+
+    return render(request, 'karty/menucard_list.html', {'menucards': menucards})
 
 
 def menucard_detail(request, menucard_id):
